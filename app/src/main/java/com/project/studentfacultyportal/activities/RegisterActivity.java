ed067a3c5848843236cec24b67524425b1843e73
Preet Gadhiya <gadhiyapreet@gmail.com>
2019-01-28 10:32:25 +0530

    First Commit

--- app/src/main/java/com/project/studentfacultyportal/activities/RegisterActivity.java
+++ app/src/main/java/com/project/studentfacultyportal/activities/RegisterActivity.java
@@ -48,7 +48,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         initListeners();
         initObjects();
     }
-
+    //Git
     /**
      * This method is to initialize views
      */
